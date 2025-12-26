8fba01d302d98b99d79edf7d4f8735fc39e78be3
Jerome <mariyajerome159@gmail.com>
2025-12-26 21:26:32 +0530

    Fix use client order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-export const dynamic = "force-dynamic";
+
 
 "use client";
-
+export const dynamic = "force-dynamic";
 import { useQuery, useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { useState } from "react";
